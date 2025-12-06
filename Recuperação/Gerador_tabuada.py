f8a34253f802a4399081f7bec287ce6d7b46e752
caio180 <caio.lima.viana2009@gmail.com>
2025-12-06 19:34:01 -0300

    Atualização

--- Recuperação/Gerador_tabuada.py
+++ Recuperação/Gerador_tabuada.py
@@ -1,4 +1,8 @@
 numero_tabuada = int(input("Digite o tipo de tabuada que desejar do 1 a 10: "))
 multiplicador = 0
-for multiplicador in range(11):
- print(numero_tabuada, " x " , multiplicador, " = ", (numero_tabuada*multiplicador))
+
+if numero_tabuada < 0:
+        print("Tente novamente.")
+else:
+    for multiplicador in range(11):
+        print(numero_tabuada, " x " , multiplicador, " = ", (numero_tabuada*multiplicador))
